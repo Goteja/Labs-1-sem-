f0ed59defcdf9a044b740f3170ed0d4b72dff29e
Tenvers <48120717+Goteja@users.noreply.github.com>
2023-01-14 16:06:41 +0300

    3.1 (Удалить повторяющиеся символы в строке)

---
+++ 3.1 (Удалить повторяющиеся символы в строке)/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+#define max 101
+
+int main(void) {
+    int t;
+    char str[max], a[max];
+    gets(str);
+    t = 1;
+    a[0] = str[0];
+    for (int i = 1; i <= max; i++){
+        if (str[i] != str[i - 1]){
+            a[t] = str[i];
+            a[t+1] = '/0';
+            t ++;
+        }
+    }
+    for (int i = 0; i <= max; i++){
+        if (a[i] == 0) break;
+        printf("%c", a[i]);
+    }
+
+}
